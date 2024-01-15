8768d5714b5139fe5bf5ee30e29cfca3c071a11e
unknown <wogjs5656@gmail.com>
2024-01-15 14:46:23 +0900

    리뷰 삭제 기능 대신 토글방식이 아닌 prompt입력방식

--- detail/script.js
+++ detail/script.js
@@ -16,7 +16,7 @@ const renderReviewCard = async () => {
         <p>리뷰 내용: ${review}</p>
         <p>확인 비밀번호: ${pw}</p>
         <button class="reviewUpdate dataBtn" >리뷰 수정</button>
-        <button class="submitReviewBtn dataBtn">리뷰 삭제</button>
+        <button class="deleteReviewBtn" name=${review_id}>리뷰 삭제</button>
         <div class="updateContainer">
           <input class="reviewInput" placeholder="리뷰 값 작성" name="reviewInput" value="${review}"/>
           <input class="pwInput" placeholder="비밀번호" name="pwInput" />
@@ -48,6 +48,20 @@ const addEvent = async () => {
     await updateReview(uuid, pw, content)
     await renderReviewCard();
   })
+
+// deleteReviewBtn
+  document.querySelector('.deleteReviewBtn').addEventListener('click', async () => {
+    const review_id = document.querySelector('.deleteReviewBtn').name;
+    const pw = prompt("리뷰를 삭제하려면 확인 비밀번호를 입력하세요.");
+
+    if (pw !== null && pw.trim() !== "") {
+      await deleteReview(review_id, pw);
+      await renderReviewCard();
+    } else {
+      alert("값이 입력되지 않았습니다.")
+    }
+  });
+
 }
 
 const submitReview = async () => {
@@ -96,10 +110,17 @@ let pw_delete = document.querySelector("input");
  * delete Review
  * @param {string} pw - value of pw_delete
  */
-const deleteReview = async (pw) => {
+const deleteReview = async (uuid, pw) => {
   let prevReview = await getReview();
-  let deletedReivew = prevReview.filter((review) => review.pw !== pw);
-  localStorage.review = JSON.stringify([...deletedReivew]);
+  let reviewToDelete = prevReview.find((review) => review.review_id === uuid);
+
+  if (reviewToDelete && reviewToDelete.pw === pw){
+    let deletedReivew = prevReview.filter((review) => review.review_id === uuid);
+    localStorage.review = JSON.stringify([deletedReivew]);
+  } else {
+    alert("해당 비밀번호가 일치하지 않습니다.")
+  }
+  
 };
 
 /** UPDATE Review */
