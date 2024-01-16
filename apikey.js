4858f17038ca3b54db9f49abcefc9b7af33f2bb7
ghi3621 <154965590+ghi3621@users.noreply.github.com>
2024-01-16 10:04:46 +0900

    Delete apikey.js

--- apikey.js
+++
@@ -1,4 +0,0 @@
-const config = {
-    apikey: "Bearer ",
-    accessToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZGQxZjM0MDg0ZjUyYzA0ZGYyOTM3MTYyNjZlMzI1OSIsInN1YiI6IjY1OGUxOTI3NjRmNzE2MGFjZDNmNTBmYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.K6jzQUO3-0t0vq93dO_JAZQuyr5iD2T2VDys5owyNUw',
-  };
